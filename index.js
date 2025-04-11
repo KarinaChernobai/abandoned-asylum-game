b8a3cdfaea1a4ef21da0c80434f819e87ffba315
Karina Chernobai <karina.chernobai@gmail.com>
2025-04-11 11:01:18 +0200

    Add key and info desk

--- index.js
+++ index.js
@@ -1,67 +1,79 @@
-
-  // Game object with all rooms
+// Game object with all rooms
 const gameObj = {
-    lobby: {
-        promptText: "You find yourself in the lobby. You have the following options: \n 1-staircase,\n 2-information desk,\n 3-elevator,\n 4-door to the left or \n 5-door to the right. \n Print your choice:",
-        options: [
-            {optionID: "4", target: "room2"}, 
-            {optionID: "5", target: "room1"}, 
-            {optionID: "1", target: "secondFloor"},
-            {optionID: "2", target: "informationDesk"},
-            {optionID: "3", target: "elevator"}
-        ]
+  lobby: {
+    promptText: "You find yourself in the lobby. You have the following options: \n 1-staircase,\n 2-information desk,\n 3-elevator,\n 4-door to the left or \n 5-door to the right. \n Print your choice:",
+    options: [
+      { optionID: "4", target: "room2" },
+      { optionID: "5", target: "room1" },
+      { optionID: "1", target: "secondFloor" },
+      { optionID: "2", target: "informationDesk" },
+      { optionID: "3", target: "elevator" }
+    ]
+  },
+  // Anastasia
+  room1: {
+    isCustomRoom: true,
+    enter: function () {
+      doctorRoom(); //
+      enterRoom(gameObj.lobby); //
     },
-    // Anastasia
-    room1: {
-        isCustomRoom: true,
-        enter: function () {
-          doctorRoom(); //
-          enterRoom(gameObj.lobby); //
-        },
-      },
-    room2: {
-        promptText: "You are in the room 2. You hear a noise behind the wall. What do you want to do? Your options are:\n 1-explore or \n 2-run back. \n What do you want to do?",
-        options: [{optionID:"1", target: "bookCase"}, {optionID:"2", target: "lobby" }]
-    },
-    bookCase: {
-        promptText: "You found a door to a secred room below the bookcase. There are still strange noises. Do you want to open door? Your options are:\n 1-check the bookcase or \n 2-go back. \n What do you want to do?",
-        options: [{optionID:"1", target: "secretRoom1"}, {optionID:"2", target: "lobby" }]
-    },
-    room3: {
-        promptText: "You are in the patient's ward. You have the following option:\n 1-go back",
-        options: [{optionID: "1", target: "lobby"}]
-    },
-    room4: {
-        promptText: "You are in the lobotomy room. Everything is bloody and you see a shadow emerging from the corner. You have only one option:\n 1-run away",
-        options: [{optionID: "1", target: "loseroom"}]
-    },
-    elevator: {
-        promptText: "You are in the elevator. You have the following options:\n 1-go back, \n 2-go upstairs, \n 3-go downstairs.\n What do you want to do?",
-        options: [{optionID: "1", target: "lobby"}, {optionID: "2", target: "secondFloor"}, {optionID: "3", target: "cellar"}]
-    },
-    cellar: {
-        promptText: "You are in the cellar. You have the following option:\n 1-go back",
-        options: [{optionID: "1", target: "lobby"}]
-    },
-    secretRoom1: { promptText: "You are in the secret room now. There is a chained ghost in the corner. It asks for help. Do you want to help it? your options are:\n 1-help the ghost or \n 2-run away. \n What do you want to do?",
-        options: [{optionID:"1", item: "key", message:"Ghost gave you a key. This is a key to another secred room where you will find you loved one.", target:"secretRoom2"}, {optionID:"2", target: "lobby" }] 
+  },
+  room2: {
+    promptText: "You are in the room 2. You hear a noise behind the wall. What do you want to do? Your options are:\n 1-explore or \n 2-run back. \n What do you want to do?",
+    options: [{ optionID: "1", target: "bookCase" }, { optionID: "2", target: "lobby" }]
+  },
+  bookCase: {
+    promptText: "You found a door to a secred room below the bookcase. There are still strange noises. Do you want to open door? Your options are:\n 1-check the bookcase or \n 2-go back. \n What do you want to do?",
+    options: [{ optionID: "1", target: "secretRoom1" }, { optionID: "2", target: "lobby" }]
+  },
+  room3: {
+    promptText: "You are in the patient's ward. You have the following option:\n 1-go back",
+    options: [{ optionID: "1", target: "lobby" }]
+  },
+  room4: {
+    promptText: "You are in the lobotomy room. Everything is bloody and you see a shadow emerging from the corner. You have only one option:\n 1-run away",
+    options: [{ optionID: "1", target: "loseroom" }]
+  },
+  elevator: {
+    promptText: "You are in the elevator. You have the following options:\n 1-go back, \n 2-go upstairs, \n 3-go downstairs.\n What do you want to do?",
+    isBlocked: true,
+    gameText: "You need a key.",
+    options: [{ optionID: "1", target: "lobby" }, { optionID: "2", target: "secondFloor" }, { optionID: "3", target: "cellar" }]
+  },
+  cellar: {
+    promptText: "You are in the cellar. You have the following option:\n 1-go back",
+    options: [{ optionID: "1", target: "lobby" }]
+  },
+  secretRoom1: {
+    promptText: "You are in the secret room now. There is a chained ghost in the corner. It asks for help. Do you want to help it? your options are:\n 1-help the ghost or \n 2-run away. \n What do you want to do?",
+    options: [{ optionID: "1", item: "key", message: "Ghost gave you a key. This is a key to another secred room where you will find you loved one.", target: "secretRoom2" }, { optionID: "2", target: "lobby" }]
 
+  },
+  secretRoom2: {
+    promptText: "You are in the secret room 2. You see a big monster and an ax on the ground. Your options are:\n 1- Get the ax and fight or \n 2- Run away. \n What do you want to do?",
+    options: [{ optionID: "Get the ax and fight", target: "winroom" }, { optionID: "run away", target: "lobby" }]
+  },
+  winroom: {
+    gameText: "You won the game. You saved your loved one. You are happy.",
+  },
+  loseroom: {
+    gameText: "You are too slow. The demon is closing behind and you feel its claws tearing at your back... Then there is darkness...",
+  },
+  informationDesk: {
+    promptText: "On the desk you see a key. Your options are:\n  1-pick key or \n 2-go back. \n What do you want to do?",
+    options: [{ optionID: "1", target: "action" }, { optionID: "2", target: "lobby" }]
+  },
+  secondFloor: {
+    promptText: "You are at the second floor. Your options are:\n  1-lobotomy room, \n 2-patient's ward or \n 3-go downstairs. \n What do you want to do?",
+    options: [{ optionID: "1", target: "room4" }, { optionID: "2", target: "room3" }, { optionID: "3", target: "lobby" }]
+  },
+  action:{
+    isCustomRoom: true,
+    enter: ()=> {
+      gameObj.elevator.isBlocked = false;
+      enterRoom(gameObj.lobby);
     },
-    secretRoom2: {
-        promptText: "You are in the secret room 2. You see a big monster and an ax on the ground. Your options are:\n 1- Get the ax and fight or \n 2- Run away. \n What do you want to do?",
-        options: [{optionID:"Get the ax and fight", target:"winroom"}, {optionID:"run away", target: "lobby" }]
-    },
-    winroom: {
-        gameText: "You won the game. You saved your loved one. You are happy.",
-    },
-    loseroom: {
-        gameText: "You are too slow. The demon is closing behind and you feel its claws tearing at your back... Then there is darkness...",
-    },
-    informationDesk: {},
-    secondFloor: {
-        promptText: "You are at the second floor. Your options are:\n  1-lobotomy room, \n 2-patient's ward or \n 3-go downstairs. \n What do you want to do?",
-        options: [{optionID: "1", target: "room4"}, {optionID: "2", target: "room3"}, {optionID: "3", target: "lobby"}]
-    }
+  }
 };
 
 function askYesNoSmart(question) {
@@ -177,6 +189,12 @@ function enterRoom(room) {
   }
 
   if (room.promptText) {
+    console.dir(room);
+    if(room.isBlocked){
+      alert(room.gameText);
+      console.log(room.options[0]);
+      return enterRoom(gameObj.lobby);
+    }
     const choice = prompt(room.promptText);
     for (let option of room.options || []) {
       if (option.optionID.toLowerCase() === choice.toLowerCase()) {
